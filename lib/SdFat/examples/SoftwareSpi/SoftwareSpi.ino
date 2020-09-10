af6808f7ba2f7ba54f4d263fa701cd39476ea95e
Mathias Skouman Völcker <mavol15@student.sdu.dk>
2020-09-10 17:07:09 +0200

    Writing to SD card now working with software SPI

--- lib/SdFat/examples/SoftwareSpi/SoftwareSpi.ino
+++ lib/SdFat/examples/SoftwareSpi/SoftwareSpi.ino
@@ -1,13 +1,17 @@
-#include <Arduino.h>
+// An example of the SdFatSoftSpi template class.
+// This example is for an Adafruit Data Logging Shield on a Mega.
+// Software SPI is required on Mega since this shield connects to pins 10-13.
+// This example will also run on an Uno and other boards using software SPI.
+//
 #include <SPI.h>
-#include <string.h>
 #include "SdFat.h"
 #if ENABLE_SOFTWARE_SPI_CLASS  // Must be set in SdFat/SdFatConfig.h
-
+//
 // Pin numbers in templates must be constants.
-const uint8_t SOFT_MISO_PIN = 26;
-const uint8_t SOFT_MOSI_PIN = 14;
-const uint8_t SOFT_SCK_PIN  = 15;
+const uint8_t SOFT_MISO_PIN = 12;
+const uint8_t SOFT_MOSI_PIN = 11;
+const uint8_t SOFT_SCK_PIN  = 13;
+//
 // Chip select may be constant or RAM variable.
 const uint8_t SD_CHIP_SELECT_PIN = 10;
 
@@ -17,14 +21,17 @@ SdFatSoftSpi<SOFT_MISO_PIN, SOFT_MOSI_PIN, SOFT_SCK_PIN> sd;
 // Test file.
 SdFile file;
 
-String DATA;
-
 void setup() {
+  Serial.begin(9600);
+  // Wait for USB Serial 
+  while (!Serial) {
+    SysCall::yield();
+  }
+  Serial.println("Type any character to start");
+  while (!Serial.available()) {
+    SysCall::yield();
+  }
 
-  Serial.begin(112500);   //  Init serial connection to main mcu
-  Serial1.begin(115200);  //  Init serial connection for debug
-  _delay_ms(10);          // Short delay to allow serial connections to get ready 
-  
   if (!sd.begin(SD_CHIP_SELECT_PIN)) {
     sd.initErrorHalt();
   }
@@ -32,36 +39,20 @@ void setup() {
   if (!file.open("SoftSPI.txt", O_RDWR | O_CREAT)) {
     sd.errorHalt(F("open failed"));
   }
-
   file.println(F("This line was printed using software SPI."));
 
   file.rewind();
   
   while (file.available()) {
-    Serial1.write(file.read());
+    Serial.write(file.read());
   }
 
   file.close();
 
-  if (!file.open("DATA.txt", O_RDWR | O_CREAT)) {
-    sd.errorHalt(F("open failed"));
-  }
-
-  Serial1.println(F("Done."));
-
+  Serial.println(F("Done."));
 }
-
 //------------------------------------------------------------------------------
-
-void loop() {
-    if(Serial.available()) {
-      DATA = Serial.readStringUntil('\n');
-      file.println(DATA);
-    
-  }
-  _delay_ms(1);
-}
-
+void loop() {}
 #else  // ENABLE_SOFTWARE_SPI_CLASS
 #error ENABLE_SOFTWARE_SPI_CLASS must be set non-zero in SdFat/SdFatConfig.h
 #endif  //ENABLE_SOFTWARE_SPI_CLASS
