b7a2b243a6eb52f644849e7d7a4af47e32f3d807
Mathias Skouman Völcker <mavol15@student.sdu.dk>
2020-10-23 16:20:05 +0200

    Added consecutive filenames

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,11 @@
 #include <SPI.h>
 #include <Wire1.h>
 #include <string.h>
+#include <EEPROM.h>
 #include "SdFat.h"
 #if ENABLE_SOFTWARE_SPI_CLASS  // Must be set in SdFat/SdFatConfig.h
+
+#define FILE_BASE_NAME "PRS"
 
 struct structA
 {
@@ -46,19 +49,19 @@ structA dataA;
 structB dataB;
 structC datac;
 
+
 void receiveEvent(int bytes);
 
-// Pin numbers in templates must be constants.
+// Define pins for SPI
 const uint8_t SOFT_MISO_PIN = PIN_PC0;
 const uint8_t SOFT_MOSI_PIN = PIN_PE3;
 const uint8_t SOFT_SCK_PIN  = PIN_PC1;
-// Chip select may be constant or RAM variable.
 const uint8_t SD_CHIP_SELECT_PIN = PIN_PE2;
-
-char wire_buffer;
 
 // SdFat software SPI template
 SdFatSoftSpi<SOFT_MISO_PIN, SOFT_MOSI_PIN, SOFT_SCK_PIN> sd;
+
+char wire_buffer;
 
 // Test file.
 SdFile file;
@@ -67,14 +70,15 @@ String DATA;
 
 void setup() {
 
-  Wire1.begin(9);
-  Wire1.onReceive(receiveEvent);
+    Wire1.begin(9);
+    //Wire1.setClock(400000);
+    Wire1.onReceive(receiveEvent);
 
 
   if (!sd.begin(SD_CHIP_SELECT_PIN)) {
     sd.initErrorHalt();
   }
-
+/*
   if (!file.open("SoftSPI.txt", O_RDWR | O_CREAT)) {
     sd.errorHalt(F("open failed"));
   }
@@ -83,14 +87,33 @@ void setup() {
 
   file.rewind();
   file.close();
+*/
 
-  if (!file.open("DATA.txt", O_RDWR | O_CREAT)) {
-    sd.errorHalt(F("open failed"));
-  }
+    const uint8_t BASE_NAME_SIZE = sizeof(FILE_BASE_NAME) - 1;
+    char fileName[13] = FILE_BASE_NAME "00.csv";
+    
+    // Find an unused file name.
+    if (BASE_NAME_SIZE > 6) {
+        //error("FILE_BASE_NAME too long");
+    }
+    while (sd.exists(fileName)) {
+        if (fileName[BASE_NAME_SIZE + 1] != '9') {
+            fileName[BASE_NAME_SIZE + 1]++;
+        } 
+        else if (fileName[BASE_NAME_SIZE] != '9') {
+            fileName[BASE_NAME_SIZE + 1] = '0';
+            fileName[BASE_NAME_SIZE]++;
+        } 
+        else {
+            //error("Can't create file name");
+        }
+    }
+
+    if (!file.open(fileName, O_WRONLY | O_CREAT | O_EXCL)) {
+        //error("file.open");
+    }
 
 }
-
-//------------------------------------------------------------------------------
 
 
 void receiveEvent(int bytes) {
@@ -123,4 +146,4 @@ void loop() {
 
 #else  // ENABLE_SOFTWARE_SPI_CLASS
 #error ENABLE_SOFTWARE_SPI_CLASS must be set non-zero in SdFat/SdFatConfig.h
-#endif  //ENABLE_SOFTWARE_SPI_CLASS
+#endif
